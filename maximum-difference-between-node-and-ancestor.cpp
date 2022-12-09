433dc01dcd63f8fe7421d96341b64e29d065f921
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2022-12-09 12:13:32 +0530

    Create maximum-difference-between-node-and-ancestor.cpp
    https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/
    LC1026

---
+++ maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,60 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int
+    maxAncestorDiff(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        
+        // Maximum difference between a node and an ancestor in a tree is
+        // the max difference between that max value node and min value node 
+        // along each root to leaf path. Traverse each root to leaf path via
+        // DFS, and track the max difference between each node on the path
+        // and its maximum/minimum valued ancestor on the path
+        
+        return maxAncestorDiff(root, root->val, root->val);
+    }
+    
+private:
+    
+    int
+    maxAncestorDiff(TreeNode* currNode, int maxValSeen, int minValSeen) {
+        
+        // Current node will be the ancestor for its left and right subtrees on
+        // root to leaf path. Update the max and min ancestor value for root
+        // to leaf path to include current node
+        maxValSeen = max(maxValSeen, currNode->val);
+        minValSeen = min(minValSeen, currNode->val);
+        // Compute the max diff between max and min value node seen so
+        // far on the root to leaf path
+        int maxDiff = maxValSeen - minValSeen;
+        
+        if (!currNode->left && !currNode->right) {
+            // Reached leaf
+            return maxDiff;
+        }
+        
+        // Update diff to include left subtree
+        if (currNode->left) {
+            maxDiff = max(maxDiff, maxAncestorDiff(currNode->left, maxValSeen, minValSeen));
+        }
+        // Update diff to include right subtree
+        if (currNode->right) {
+            maxDiff = max(maxDiff, maxAncestorDiff(currNode->right, maxValSeen, minValSeen));
+        }
+        
+        return maxDiff;
+    }
+    
+};
