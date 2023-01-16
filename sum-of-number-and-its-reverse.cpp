739ebfb1f0c1be46b5ff93e9ba88f4d665fab06a
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-01-16 16:38:17 +0530

    Update sum-of-number-and-its-reverse.cpp
    Code cleanup

--- sum-of-number-and-its-reverse.cpp
+++ sum-of-number-and-its-reverse.cpp
@@ -4,17 +4,26 @@ public:
     sumOfNumberAndReverse(int num) {
         
         // Iterate over the possible values of +ve candidate numbers in
-        // the range [1, n) s.t. candidate and its reverse add upto the
+        // the range [num/2, num] s.t. candidate and its reverse add upto the
         // given number
-        for (int candidateNum = 0; candidateNum <= num; ++candidateNum) {
+        for (int cn = num / 2; cn <= num; ++cn) {
             
-            // Compute the expected value of the complement
-            // for the given candidate number (s.t the candidate
-            // and its complement add up to the specified number)
-            int expectedComplement = num - candidateNum;
-
+            // Check if candidate number and its reversed value equal the given number
             // Check if the complement is the reverse of the candidate
-            if (reversedNumbers(candidateNum, expectedComplement)) {
+            if (cn + reversedNumber(cn) == num) {
+                // Candidate number found satifying the given constraint 
+                return true;
+            }
+        }
+        
+        // Iterate over the possible values of +ve candidate numbers in
+        // the range [0, num/2] s.t. candidate and its reverse add upto the
+        // given number
+        for (int cn = num / 2; cn >= 0; --cn) {
+            
+            // Check if candidate number and its reversed value equal the given number
+            // Check if the complement is the reverse of the candidate
+            if (cn + reversedNumber(cn) == num) {
                 // Candidate number found satifying the given constraint 
                 return true;
             }
@@ -26,22 +35,17 @@ public:
     
 private:
     
-    inline bool
-    reversedNumbers(int num1, int num2) {
-        // Compute the reverse of 'num1'
-        long reverseNum1 = 0;
-        while (num1 > 0) {
-            auto num1Digit = num1 % 10;
-            num1 /= 10;
-            reverseNum1 = (reverseNum1 * 10) + num1Digit;
-            if (reverseNum1 > num2) {
-                // Additional check for fast exit
-                return false;
-            }
+    inline long
+    reversedNumber(int num) {
+        // Compute the reverse of 'num'
+        long rn = 0;
+        while (num > 0) {
+            auto d = num % 10;
+            num /= 10;
+            rn = (rn * 10) + d;
         }
         
-        // Return true if num1 is reverse of num2
-        return reverseNum1 == num2;
+        return rn;
     }
     
 };
