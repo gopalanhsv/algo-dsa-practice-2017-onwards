9da3b004135cee474a9f33b4f64d7d18c60bc52f
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-01-17 21:39:15 +0530

    Create binary-prefix-divisible-by-5.cpp
    https://leetcode.com/problems/binary-prefix-divisible-by-5/
    LC1018

---
+++ binary-prefix-divisible-by-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<bool>
+    prefixesDivBy5(vector<int>& nums) {
+        vector<bool> outV;
+        // Iterate through the binary array from L->R sequentally
+        int prefixDecNum = 0;
+        for (auto num : nums) {
+            // Construct decimal equivalent for the binary prefix
+            // seen so far(from index 0 to current index)
+            prefixDecNum <<= 1;
+            prefixDecNum += num;
+            // Since the 'nums' array can be huge, its possible for its decimal
+            // equivalent to not into standard types. Since the prefix computed
+            // in ihis iteration is always multiplied by a power of 2 to contribute
+            // to the number generated in subsequent iterations; and 2 powers are
+            // not divisible by 5; the number can be replaced by its remainder on
+            // dividing by 5 for divisiblity checks in succeeding iterations
+            prefixDecNum = prefixDecNum % 5;
+            // Check for divisibility by 5 of current prefix decimal equivalent
+            outV.push_back((0 == prefixDecNum));
+        }
+        
+        return outV;
+    }
+};
