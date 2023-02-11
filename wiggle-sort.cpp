7f66e8271eab895aa3c5a1804cd464b68014ba01
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-02-11 20:25:29 +0530

    Update wiggle-sort.cpp

--- wiggle-sort.cpp
+++ wiggle-sort.cpp
@@ -6,22 +6,21 @@ public:
         // the ones at odd indices; and elements at odd indices
         // are always >= the ones at even indices.
         
-        // So the above can be done by a linear scan of the array
-        // from left to right and swapping the elements if they
-        // fail the above criteria. At each index we take the
-        // greedy decision of ensure that the element at that
-        // index is ordered as the requirements;
+        // Achieved by a linear scan of the array from L->R and
+        // swapping the elements if they fail the above criteria.
+        // At each index adopt greedy approach to ensure that
+        // the particular element is ordered as the requirements
         for (int i = 0; i < nums.size() - 1; ++i) {
             if (i % 2 == 0) {
-                // i is even index. Elements at even index should always
-                // be lesser than the element at odd index
+                // Even index. Elements at even index should always
+                // be l.t. element at succeeding odd index
                 // Swap elements failing this condition
                 if (nums[i] > nums[i + 1]) {
                     swap(nums[i], nums[i + 1]);
                 }
             } else {
-                // i is odd index. Elements at odd index should always
-                // be greater than the element at even index
+                // Odd index. Elements at odd index should always
+                // be g.t. element at succeeding even index
                 // Swap elements failing this condition
                 if (nums[i] < nums[i + 1]) {
                     swap(nums[i], nums[i + 1]);
