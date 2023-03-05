68c0eaab40b17248baf0ec8eed60972dc0784bf2
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-03-05 12:09:38 +0530

    Update jump-game.cpp

--- jump-game.cpp
+++ jump-game.cpp
@@ -8,21 +8,20 @@ public:
         }
         
         int arrSz = nums.size();
-        // Tracks the nearest index to right from current position
-        // fromt where it is possible to reach last index
-        int closestReachableRightIdx = arrSz - 1;
+        // Tracks the leftmost from where it is possible to reach last index
+        int leftmostIdx = arrSz - 1;
         // Iterate over each of the index locations from R to L sequentially
         for (int currIdx = arrSz - 2; currIdx >= 0; --currIdx) {
-            // Compute the position of the maximum allowable array index
+            // Compute the position of the rightmost allowable array index
             // which can be reached via jumping from current position
             int maxReachableIdxToRight = currIdx + nums[currIdx];
-            if (maxReachableIdxToRight >= closestReachableRightIdx) {
+            if (maxReachableIdxToRight >= leftmostIdx) {
                 // Possible to reach the array index from current location
-                closestReachableRightIdx = currIdx;
+                leftmostIdx = currIdx;
             }
         }
         
         // Return true if possible to reach array end from array start
-        return 0 == closestReachableRightIdx;
+        return 0 == leftmostIdx;
     }
 };
