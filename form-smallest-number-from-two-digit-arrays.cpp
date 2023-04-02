697fd4611a2264fba4a8aadc71a3d653ff2cbe14
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-04-02 23:38:56 +0530

    Create form-smallest-number-from-two-digit-arrays.cpp
    https://leetcode.com/problems/form-smallest-number-from-two-digit-arrays/
    LC2605

---
+++ form-smallest-number-from-two-digit-arrays.cpp
@@ -0,0 +1,48 @@
+// Time compexity : O(max(nums1.size() + nums2.size()))
+// Space complexity : O(1)
+
+class Solution {
+public:
+    int
+    minNumber(vector<int>& nums1, vector<int>& nums2) {
+        // Since both 'nums1' and 'nums2' consist of only single digit
+        // numbers, the smallest number that contains at least one digit
+        // from each would be one of the following :-
+        // 1. Single digit number which is present in both
+        //    'nums1' and 'nums2'; OR
+        // 2. Two digit number which consist of the minimum digit in
+        //    'nums1' and the minimum digit in 'nums2' (with the smallest
+        //     of the selected digits in tens location and the other in
+        //     the units location)
+        
+        // Find min digit in 'nums1'
+        int minDigitInNums1 = nums1[0];
+        vector<bool> nums1DigitTbl(10, false);
+        for (auto & d : nums1) {
+            minDigitInNums1 = min(d, minDigitInNums1);
+            nums1DigitTbl[d] = true;
+        }
+        
+        // Find min digit in 'nums1' and min common digit across both 'nums1' & 'nums2'
+        int minDigitInBothNums1AndNums2 = 10;
+        int minDigitInNums2 = nums2[0];
+        for (auto & d : nums2) {
+            minDigitInNums2 = min(d, minDigitInNums2);
+            if (nums1DigitTbl[d]) {
+                // Current digit in both 'nums1' & 'nums2'
+                // Update the min common digit across 'nums1' & 'nums2'
+                minDigitInBothNums1AndNums2 = min(minDigitInBothNums1AndNums2, d);
+            }
+        }
+        
+        // Option 1
+        if (10 != minDigitInBothNums1AndNums2) {
+            return minDigitInBothNums1AndNums2;
+        }
+        
+        // Option 2
+        return 
+            (min(minDigitInNums1, minDigitInNums2) * 10) + 
+            (max(minDigitInNums1, minDigitInNums2));
+    }
+};
