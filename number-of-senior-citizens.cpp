a871fa115469771f7b8645468f3ed03a4aa17d4e
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-05-15 22:43:28 +0530

    Create number-of-senior-citizens.cpp
    https://leetcode.com/problems/number-of-senior-citizens/
    LC2678

---
+++ number-of-senior-citizens.cpp
@@ -0,0 +1,38 @@
+// Time complexity : O(details.size())
+// Space complexity : O(details.size())
+
+class Solution {
+public:
+    int
+    countSeniors(vector<string>& details) {
+        // Since seat/phone numbers are distinct, use either of the
+        // two to filter out duplicate entries. Use the seat number
+        // as it has only 2 digits (for conversion to decimal)
+        // 'Seat number' at chars 13, 14
+        const int seatIdxOffset = 13;
+        // 'Age' at chars 11, 12
+        const int ageIdxOffset = 11;
+        int nSeniors = 0;
+        // Hash table of senior passengers already processed
+        unordered_set<int> seenSeatsTbl;
+        // Process each passenger
+        for (auto & detail : details) {
+            auto age = 
+                int(detail[ageIdxOffset] - '0') * 10 + int(detail[ageIdxOffset + 1] - '0');
+            if (age <= 60) {
+                // Filter out non-seniors
+                continue;
+            }
+            // Filter out duplicate senior passsengers based on seat numbers
+            auto seatId =
+                int(detail[seatIdxOffset] - '0') * 10 + int(detail[seatIdxOffset + 1] - '0');
+            if (seenSeatsTbl.find(seatId) == seenSeatsTbl.end()) {
+                // New senior passenger
+                seenSeatsTbl.insert(seatId);
+                ++nSeniors;
+            }
+        }
+        
+        return nSeniors;
+    }
+};
