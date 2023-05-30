07b3c63657db185a5377df3e7ca6cae96e19d329
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-05-30 20:45:32 +0530

    Create buy-two-chocolates.cpp
    https://leetcode.com/problems/buy-two-chocolates/
    LC2706

---
+++ buy-two-chocolates.cpp
@@ -0,0 +1,25 @@
+// Time complexity : O(prices.size())
+// Space complexity : O(1)
+class Solution {
+public:
+    int 
+    buyChoco(vector<int>& prices, int money) {
+        // To minimize the spend on the chocolates, select 2 of the
+        // least priced chocos (i.e first and second min)
+        int firstMin = numeric_limits<int>::max();
+        int secondMin = numeric_limits<int>::max();
+        for (auto & price : prices) {
+            if (price <= firstMin) {
+                secondMin = firstMin;
+                firstMin = price;
+            } else if (price < secondMin) {
+                secondMin = price;
+            }
+        }
+        
+        // Compute leftover money
+        auto leftover = money - (firstMin + secondMin);
+        // Leftover leading to debt => no buy possible
+        return (leftover < 0) ? money : leftover;
+    }
+};
