b5e4d6c941d0c909d254a58580649bb7d06d94eb
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-09-09 00:24:26 +0530

    Create maximum-sum-score-of-array.cpp
    https://leetcode.com/problems/maximum-sum-score-of-array/
    LC2219

---
+++ maximum-sum-score-of-array.cpp
@@ -0,0 +1,33 @@
+// n => nums.size()
+// Time complexity : O(n)
+// Space complexity : O(n)
+class Solution {
+public:
+    long long
+    maximumSumScore(vector<int>& nums) {
+        // Sum score at each index is the max of the prefix sum and suffix
+        // sum at that index (provided both sums include the index element)
+        int arrSz = nums.size();
+        // Build the suffix sum array for the input array
+        vector<long long> suffixSumV(arrSz, 0);
+        suffixSumV[arrSz - 1] = nums[arrSz - 1];
+        for (int i = arrSz - 2; i >= 0; --i) {
+            auto & currElem = nums[i];
+            suffixSumV[i] = currElem + suffixSumV[i + 1];
+        }
+        
+        // Iterate over the array elements from L->R sequentially
+        long long prefixSum = 0;
+        long long maxSumScore = numeric_limits<long long>::min();
+        for (int i = 0; i < arrSz; ++i) {
+            auto & currElem = nums[i];
+            // Compute prefix sum at current index
+            prefixSum += currElem;
+            // Compute the sum score at current index and update the maximum sum score
+            long long currIdxSumScore = max(prefixSum, suffixSumV[i]);
+            maxSumScore = max(maxSumScore, currIdxSumScore);
+        }
+        
+        return maxSumScore;
+    }
+};
