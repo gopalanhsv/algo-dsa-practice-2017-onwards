3fa5f7e0bd3ede97e7379e8c06479191eda31b3b
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2023-11-29 08:32:21 +0530

    Update max-consecutive-ones.cpp

--- max-consecutive-ones.cpp
+++ max-consecutive-ones.cpp
@@ -1,47 +1,40 @@
+// n => v.size()
+// Time complexity : O(n)
+// Space complexity : O(1)
+
 class Solution {
 public:
     int
     findMaxConsecutiveOnes(vector<int>& v) {
-        // Empty input vector
-        if (v.empty()) {
+        auto arrSz = v.size();
+
+        if (0 == arrSz) {
+            // Base case of empty input vector
             return 0;
         }
-        
-        typedef vector<int>::size_type vecSzT;
-        // Skip leading zeroes till the first 1 is found. Commence
-        // a sliding expanding window from this point
-        vecSzT winStart = 0;
-        while (winStart < v.size() && v[winStart] != 1) {
-            ++winStart;
-        }
 
-        // Examine all elements of vector sequentially
-        vecSzT maxWinSize = 0;
-        for (vecSzT winEnd = winStart; winEnd < v.size(); ++winEnd) {
-            
-            // Current element is a zero,
-            if (v[winEnd] == 0) {
-                
-                // Previous window of ones gets terminated
-                // Now adjust start of window by skipping zeroes till a one
-                // is reached (start of next window) or vector terminates
-                winStart = winEnd;
-                while (winStart < v.size() && v[winStart] != 1) {
-                    ++winStart;
+        auto wStart = -1;
+
+        // Iterate over the array from L->R sequentially
+        auto maxWinSize = 0;
+        for (auto wEnd = 0; wEnd < arrSz; ++wEnd) {
+            if (0 == v[wEnd]) {
+                // Current element is a zero
+                // Terminate any running window of consecutive ones
+                wStart = -1;
+            } else {                
+                // Current element is a one
+                if (-1 == wStart) {
+                    // Commence a window of ones at this location if none
+                    // continues from previously examined locations
+                    wStart = wEnd;
                 }
-                winEnd = winStart;
-                
-            } else {
-                
-                // Current element is a one, expand window
-                // Compute its size and update max window size. 
-                vecSzT winSize = winEnd - winStart + 1;
-                maxWinSize = max(maxWinSize, winSize);
+                // Compute window size and update max window size
+                auto winSz = wEnd - wStart + 1;
+                maxWinSize = max(maxWinSize, winSz);
             }
-            
         }
         
         return maxWinSize;
-        
     }
 };
