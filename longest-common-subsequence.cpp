acb975408ee5c5825bec0bd50486c8da99ef03de
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2024-01-25 11:25:37 +0530

    Update longest-common-subsequence.cpp

--- longest-common-subsequence.cpp
+++ longest-common-subsequence.cpp
@@ -1,3 +1,12 @@
+// m = > text1.size(), n => text2.size()
+// DP Approach
+// Time complexity : O(mn)
+// Space complexity : O(mn)
+
+// DP Approach Space Optimized
+// Time complexity : O(mn)
+// Space complexity : O(min(mn))
+
 class Solution {
 public:
     int
@@ -17,21 +26,21 @@ private:
     int
     lcsDPApproach(string& text1, string& text2) {
         // String lengths
-        int len1 = text1.size();
-        int len2 = text2.size();
+        auto len1 = text1.size();
+        auto len2 = text2.size();
         
-        // dpTbl(i, j) is the length of LCS of the two substrings
+        // dpTbl(i, j) is the LCS len of the two substrings
         // text1[0, i - 1] & text2[0, j - 1]
         vector<vector<int> > dpTbl(len1 + 1, vector<int>(len2 + 1, 0));
         // Compare the two input strings one char at a time
         
-        // Current substring of text1 is text1[0, i - 2]. 
-        // Adding char text1[i - 1] to the substring 
-        for (int i = 1; i <= len1; ++i) {
-            
-            // Current substring of text2 is text2[0, j - 2]. 
-            // Adding char text2[j - 1] to the substring 
-            for (int j = 1; j <= len2; ++j) {
+        // Iterate over text1 chars from L->R sequentially
+        // Add char text1[i - 1] to the substring text1[0, i - 2]
+        for (auto i = 1; i <= len1; ++i) {
+
+            // Iterate over text2 chars from L->R sequentially
+            // Add char text2[j - 1] to the substring text2[0, j - 2]
+            for (auto j = 1; j <= len2; ++j) {
                 
                 // Assuming that the new char added to both the substrings
                 // of text1 and text2 do not match; LCSLen(i, j) is initialized
@@ -43,8 +52,8 @@ private:
                                   max(dpTbl[i - 1][j], dpTbl[i][j - 1]));
                 
                 if (text1[i - 1] == text2[j - 1]) {
-                    // New char being added to both the substrings is equal;
-                    // so len of common subsequence is updated by 1 to that
+                    // New char being added to both the substrings matches
+                    // => len of common subsequence is updated by 1 to that
                     // of the subsequence formed between earlier substrings
                     // text1[0, i - 2] & text2[0, j - 2]
                     // Update the LCSLen(i, j) to the max possible value
@@ -60,25 +69,25 @@ private:
     int
     lcsDPApproachSpaceOptimized(string& text1, string& text2) {
         // String lengths
-        int len1 = text1.size();
-        int len2 = text2.size();
+        auto len1 = text1.size();
+        auto len2 = text2.size();
         
         // Technique is similar to above except that a space optimized
         // DP table is used which consists of just 2 rows; as calc for 
         // each row depends just on the previous row values
         
-        // dpTbl(i, j) is the length of LCS of the two substrings
+        // dpTbl(i, j) is the LCS len of the two substrings
         // text1[0, i - 1] & text2[0, j - 1]
         vector<vector<int> > dpTbl(2, vector<int>(len2 + 1, 0));
         // Compare the two input strings one char at a time
-        
-        // Current substring of text1 is text1[0, i - 2]. 
-        // Adding char text1[i - 1] to the substring 
-        for (int i = 1; i <= len1; ++i) {
+
+        // Iterate over text1 chars from L->R sequentially
+        // Add char text1[i - 1] to the substring text1[0, i - 2]
+        for (auto i = 1; i <= len1; ++i) {
             
-            // Current substring of text2 is text2[0, j - 2]. 
-            // Adding char text2[j - 1] to the substring 
-            for (int j = 1; j <= len2; ++j) {
+            // Iterate over text2 chars from L->R sequentially
+            // Add char text2[j - 1] to the substring text2[0, j - 2]
+            for (auto j = 1; j <= len2; ++j) {
                 
                 // Assuming that the new char added to both the substrings
                 // of text1 and text2 do not match; LCSLen(i, j) is initialized
