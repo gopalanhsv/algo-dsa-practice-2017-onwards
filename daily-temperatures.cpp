8dd09f8ac60fa8452434fe20e22c57f62f2f87bb
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2024-01-31 16:00:10 +0530

    Update daily-temperatures.cpp

--- daily-temperatures.cpp
+++ daily-temperatures.cpp
@@ -1,21 +1,27 @@
+// n => temperatures.size()
+// Time complexity : O(n)
+// Space complexity : O(n)
 class Solution {
 public:
     vector<int>
     dailyTemperatures(vector<int>& temperatures) {
         
-        // Iterate over the temperature vector from L to R.
-        int n = temperatures.size();
+        // Problem reduces to finding the location of next greater element
+        // for each element (easily done via a monotonic stack)
+        auto n = temperatures.size();
         
-        // Monostack storing indices of temperature vector whose
-        // warmer temperature are yet to be determined
+        // Monostack storing indices of elements in the array whose
+        // next warmer temperatures are yet to be determined
         stack<int> stk;
         vector<int> distanceToWarmerTempV(n, 0);
-        for (int i = 0; i != n; ++i) {
+        // Iterate over the temperature vector from L to R sequentially
+        for (auto i = 0; i != n; ++i) {
 
-            int currTemp = temperatures[i];
+            auto currTemp = temperatures[i];
             
-            // For all indices to the left whose warmer temp is not determined;
-            // and whose temperature is lesser than current, update distances
+            // For all indices to the left whose warmer temp is not yet determined
+            // and whose temperature is lesser than current, the current temperature
+            // becomes the next warmer temperature
             while (!stk.empty() && 
                    (currTemp > temperatures[stk.top()]) ) {
                 distanceToWarmerTempV[stk.top()] = i - stk.top();
@@ -23,7 +29,7 @@ public:
             }
                    
             // Add index of current element to stack top, as its warmer temperature
-            // to right is yet to be examined.
+            // to right is yet to be determined
             stk.push(i);
         }
                    
