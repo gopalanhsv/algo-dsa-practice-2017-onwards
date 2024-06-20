cfb4434d5babd5053151af6f01957644fd4c6597
iamshyam <115415870+gopalanhsv@users.noreply.github.com>
2024-06-20 18:43:09 +0530

    Update magnetic-force-between-two-balls.cpp

--- magnetic-force-between-two-balls.cpp
+++ magnetic-force-between-two-balls.cpp
@@ -1,3 +1,7 @@
+// n => position.size(), M = max_element(position)
+// Time complexity : O(nlogM + nlogn) 
+// Space complexity : O(logn)
+
 class Solution {
 public:
     int
@@ -9,32 +13,31 @@ public:
         // in baskets at extreme/end positions
         int maxForce = position[position.size() - 1] - position[0];
         // Minimum value of magnetic force is between balls placed
-        // in adjacent baskets hypothetically
+        // in closest baskets
         int minForce = 1 ; //position[1] - position[0];
         //for (int i = 2; i < position.size(); ++i) {
         //    minForce = min(minForce, position[i] - position[i - 1]);
         //}
         
-        // Execute a binary search for the maximum magnetic force in
-        // the range [minForce, maxForce] which allows 'm' balls to be distributed
-        // in the given baskets. Basically, do a binary search for the maximum
-        // distance 'x' in the range [minForce, maxForce], which allows placement of
-        // all 'm' balls in baskets with the minimum distance (minimum magentic
-        // force) between each ball being 'x'
+        // Execute a binary search for the maximum magnetic force in the range
+        // [minForce, maxForce] which allows 'm' balls to be distributed in the given
+        // baskets. That is, do a binary search for the maximum allowable distance 'x'
+        // in the range [minForce, maxForce], enabling placement of all 'm' balls
+        // in baskets with the minimum distance (minimum magentic force) between each
+        // ball being 'x'
         int l = minForce, r = maxForce;
         while (l < r) {
-            // l + 1 stunt for convergence. Always use with l = mid setting
+            // l + 1 stunt for convergence. Reqd for l = mid setting
             int mid = l + 1 + (r - l) / 2;
             if (possibleToPlaceAllBallsInBaskets(position, mid, m)) {
                 // Possible to distribute 'm' balls in baskets with
                 // a distance of 'mid' in between
-                // This sets the lower limit for search range and becomes
-                // the minimum
+                // Set the lower bound for search range
                 l = mid;
             } else {
                 // Not possible to distribute 'm' balls in baskets with
                 // a distance of 'mid' in between
-                // Try with smaller distances in between the balls
+                // Try with reduced space between the balls
                 r = mid - 1;
             }
         }
